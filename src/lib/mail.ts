5ba6951426cba58becc42cabdf8dbd5e224d3ad9
Anish-Rai <arvik.rai3377@gmail.com>
2024-04-19 17:34:45 +0545

    third retry of deployment

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -24,7 +24,7 @@ export const sendPasswordResetEmail = async(
   const resetLink = `${domain}/auth/new-password?token=${token}`
 
         await resend.emails.send({
-          from: 'onboarding@resend.dev',
+          from: 'anish@rai.dev',
           to: email,
           subject: "Reset Your Password",
           html: `<p> Click <a href="${resetLink}"> here </a> to reset your password </p>`
@@ -38,7 +38,7 @@ export const sendVerificationEmail = async (
     const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
 
         await resend.emails.send({
-          from: 'onboarding@resend.dev',
+          from: 'anish@rai.dev',
           to: email,
           subject: "confirm your email",
           html: `<p> Click <a href="${confirmLink}"> here </a> to confirm email </p>`
